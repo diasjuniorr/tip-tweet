58847ca3696c45006cfe984359fef5f16a22b6f2
Dias Junior <reinaldo.diasjunior@gmail.com>
2022-01-17 20:57:46 +0100

    add connect to wallet button

--- front-end/pages/index.tsx
+++ front-end/pages/index.tsx
@@ -10,7 +10,11 @@ import abi from "../contracts/abi/TipTweet.json";
 
 const CONTRACT_ABI = abi;
 
+//typescript workaround
+declare let window: any;
+
 const Home: NextPage = () => {
+  const [currentAccount, setCurrentAccount] = useState("");
   const [tweetUrl, setTweetUrl] = useState("");
   const [tweetID, setTweetID] = useState("");
   const [tip, setTip] = useState("");
@@ -18,6 +22,27 @@ const Home: NextPage = () => {
   const [user, setUser] = useState<User | null>();
 
   const router = useRouter();
+
+  const checkIfWalletIsConnected = async () => {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      console.log("Make sure you have metamask!");
+      return;
+    } else {
+      console.log("We have the ethereum object", ethereum);
+    }
+
+    const accounts = await ethereum.request({ method: "eth_accounts" });
+
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+      console.log("Found an authorized account:", account);
+      setCurrentAccount(account);
+    } else {
+      console.log("No authorized account found");
+    }
+  };
 
   const handleLogOut: MouseEventHandler = async (e) => {
     e.preventDefault();
@@ -71,6 +96,42 @@ const Home: NextPage = () => {
     //save signature to the database and attach a twitter user id to it
   }
 
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // Render Methods
+  const renderNotConnectedContainer = () => (
+    <div className="flex justify-center">
+      <button
+        onClick={connectWallet}
+        className="mt-10 text-lg text-white font-semibold btn-bg py-3 px-6 rounded-md focus:outline-none focus:ring-2"
+      >
+        Connect to Wallet
+      </button>
+    </div>
+  );
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
+
   useEffect(() => {
     const getProfile = () => {
       const profile = supabase.auth.user();
@@ -105,45 +166,49 @@ const Home: NextPage = () => {
         <h1 className="text-3xl font-semibold text-center text-white">
           Tip a tweet and support the author
         </h1>
-        <div className="flex flex-col p-6">
-          <form className="flex flex-col" onSubmit={handleSubmit}>
-            <label htmlFor="tweet" className="text-gray-200">
-              Tweet URL
-            </label>
-            <input
-              className="py-2 px-4 rounded-md focus:outline-none focus:ring-2"
-              type="text"
-              id="tweet"
-              value={tweetUrl}
-              onChange={(e) => setTweetUrl(e.target.value)}
-            />
+        {currentAccount === "" ? (
+          renderNotConnectedContainer()
+        ) : (
+          <div className="flex flex-col p-6">
+            <form className="flex flex-col" onSubmit={handleSubmit}>
+              <label htmlFor="tweet" className="text-gray-200">
+                Tweet URL
+              </label>
+              <input
+                className="py-2 px-4 rounded-md focus:outline-none focus:ring-2"
+                type="text"
+                id="tweet"
+                value={tweetUrl}
+                onChange={(e) => setTweetUrl(e.target.value)}
+              />
 
-            <label htmlFor="tipAmount" className="mt-6 text-gray-200">
-              Tip Amount
-            </label>
-            <input
-              className="py-2 px-4 rounded-md focus:outline-none focus:ring-2"
-              type="text"
-              id="text"
-              value={tip}
-              onChange={(e) => setTip(e.target.value)}
-            />
+              <label htmlFor="tipAmount" className="mt-6 text-gray-200">
+                Tip Amount
+              </label>
+              <input
+                className="py-2 px-4 rounded-md focus:outline-none focus:ring-2"
+                type="text"
+                id="text"
+                value={tip}
+                onChange={(e) => setTip(e.target.value)}
+              />
 
+              <button
+                className="mt-10 text-lg text-white font-semibold btn-bg py-3 px-6 rounded-md focus:outline-none focus:ring-2"
+                type="submit"
+              >
+                Tip Tweet
+              </button>
+            </form>
+            <div className="text-center text-3xl text-white m-10">OR</div>
             <button
-              className="mt-10 text-lg text-white font-semibold btn-bg py-3 px-6 rounded-md focus:outline-none focus:ring-2"
-              type="submit"
+              className="text-lg text-white font-semibold btn-bg-2 py-3 px-6 rounded-md focus:outline-none focus:ring-2"
+              onClick={handleLogOut}
             >
-              Tip Tweet
+              Claim your tip
             </button>
-          </form>
-          <div className="text-center text-3xl text-white m-10">OR</div>
-          <button
-            className="text-lg text-white font-semibold btn-bg-2 py-3 px-6 rounded-md focus:outline-none focus:ring-2"
-            onClick={handleLogOut}
-          >
-            Claim your tip
-          </button>
-        </div>
+          </div>
+        )}
       </div>
     </div>
   );
