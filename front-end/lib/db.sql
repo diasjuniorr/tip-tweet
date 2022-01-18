4eac688f07d2734008af441717faeb204e47dc1a
Dias Junior <reinaldo.diasjunior@gmail.com>
2022-01-18 22:31:27 +0100

    refact sql

--- front-end/lib/db.sql
+++ front-end/lib/db.sql
@@ -1,25 +1,13 @@
--- Create a table for public "users"
-create table users (
-  id uuid references auth.users not null,
-  created_at timestamp with time zone not null default now(),
-  updated_at timestamp with time zone not null default now(),
-  twitter_user_id text unique,
-
-  primary key (id),
-  unique(twitter_user_id)
-);
-
 -- Create a table for "contracts"
 create table contracts (
   id uuid not null,
+  user_id uuid not null,
   created_at timestamp with time zone not null default now(),
   updated_at timestamp with time zone not null default now() ,
   deleted_at timestamp with time zone,
-  user_id uuid not null,
   address text not null,
   active boolean not null default true,
   primary key (id),
-  constraint fk_user_id foreign key (user_id) references users (id) on delete cascade  
 );
 
 
@@ -29,13 +17,14 @@ create policy "Users can select their own contracts."
   on contracts for select
   using ( auth.uid() = user_id );
 
-create policy "Users can insert their own contractws"
+create policy "Users can insert their own contracts"
   on contracts for insert
   with check ( auth.uid() = user_id );
 
 -- Create a table for "tips"
 create table tips (
   id uuid not null,
+  user_id uuid not null,
   created_at timestamp with time zone not null default now(),
   updated_at timestamp with time zone not null default now(),
   deleted_at timestamp with time zone,
@@ -43,9 +32,8 @@ create table tips (
   tweet_id text not null,
   nonce text not null,
   amount text not null,
-  twitter_user_id text not null,
+  tweet_owner_id text not null,
   signature text not null,
   primary key (id),
   constraint fk_contract_id foreign key (contract_id) references contracts (id) on delete cascade,
-  constraint fk_twitter_user_id foreign key (twitter_user_id) references users (twitter_user_id) on delete cascade
 );
