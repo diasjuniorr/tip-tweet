3a140b9a23e4403ca05d3bbe3937592a57828d09
Dias Junior <reinaldo.diasjunior@gmail.com>
2022-01-20 22:47:33 +0100

    wip add claim tip button

--- front-end/pages/tips/index.tsx
+++ front-end/pages/tips/index.tsx
@@ -161,7 +161,7 @@ const Tips: NextPage = () => {
                   </div>
                   <div className="px-6 pt-4 pb-2">
                     <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                      {(tip.amount)} ETH
+                      {(tip.amount.toString())} ETH
                     </span>
                     <button
                       onClick={() => verifySignature(tip)}
