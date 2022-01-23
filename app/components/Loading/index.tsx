e6f8ef9b59a99b745d97c7ab9ee3cfe27874a851
Dias Junior <reinaldo.diasjunior@gmail.com>
2022-01-23 19:41:31 +0100

    add loading component

--- app/components/Loading/index.tsx
+++ app/components/Loading/index.tsx
@@ -2,7 +2,7 @@ const Loading = () => {
   return (
     <div className="flex justify-center items-center">
       <div className="flex items-center justify-center ">
-        <div className="w-24 h-24 border-l-2 border-gray-900 rounded-full animate-spin"></div>
+        <div className="w-24 h-24 border-l-2 border-green-400 rounded-full animate-spin"></div>
       </div>
     </div>
   );
